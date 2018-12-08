611c56cf2a4fd20b347e1411a8d007714f5af4f6
pochun <bear110088@gmail.com>
2018-12-08 22:25:07 +0800

    complete-4

--- main.cpp
+++ main.cpp
@@ -2,17 +2,29 @@
 #include <vector>
 
 int main(){
+	/*
 	vector<vector<int>> matrix{ {4,1,6,2,3},
 								{5,0,3,7,6},
 								{2,3,4,5,8},
 								{3,4,6,3,4},
 								{4,6,5,8,6}
 							  };
-	int a;
-	cin >> a;
-	cout << a;
+	*/
+	vector<vector<int>> matrix;
+	int n;
+	cin >> n;
 
-	HungarianMatrix H(5,matrix);
+	vector<int> temp(n, 0);
+	for(int i=0; i<n; i++){
+		matrix.push_back(temp);
+	}
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			cin >> matrix[i][j];
+		}
+	}
+
+	HungarianMatrix H(n,matrix);
 	vector<int> R;
 	R=H.get_PerfectMatching();
 	int weight=0;
